b12fe0a8f6e35fe9261a409d0fb6f5c73640871a
KristynaSodomkova <56554752+KristynaSodomkova@users.noreply.github.com>
2021-02-22 21:35:06 +0100

    Vyřešení úkolu 5

--- 1/ukoly/ukol05.py
+++ 1/ukoly/ukol05.py
@@ -14,7 +14,7 @@ prodeje2019 = {
 }
 
 prodeje2020 = {
-    "Zkus mě chytit": 3157,
+    # "Zkus mě chytit": 3157,
     "Vrah zavolá v deset": 3541,
     "Vražda podle knihy": 2510,
     "Past": 2364,
@@ -22,22 +22,18 @@ prodeje2020 = {
     "Zkus mě chytit": 6671,
 }
 
-# prodeje2019.update(prodeje2020)
-
 
 nameBook = input("Zadejte název knihy: ")
 
+for key in prodeje2020:
+    if key in prodeje2019:
+        prodeje2020[key] = prodeje2020[key] + prodeje2019[key]
+
+prodejeCelkem = {**prodeje2019, **prodeje2020}
+
+print(prodejeCelkem[nameBook])
 
 
-# print(prodeje2019[nameBook])
-
-# prodejeSum = {}
-# for k in prodeje2019:
-#     prodejeSum[k] = prodeje2019[k]
-# for k in prodeje2020:
-#     prodejeSum[k] = prodeje2020[k]
-#
-# print(prodejeSum)
 
 
 
