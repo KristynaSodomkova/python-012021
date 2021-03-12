c1c238276f26e225f58dc97c7e8c1f279ac5cfe1
KristynaSodomkova <56554752+KristynaSodomkova@users.noreply.github.com>
2021-03-12 23:56:54 +0100

    hotovy ukol 20

---
+++ 4/ukoly4/ukol20.py
@@ -0,0 +1,20 @@
+from faker import Faker
+generator_falesnych_dat = Faker(["cs_CZ", "sk_SK"])
+
+# print(generator_falesnych_dat.name())
+# print(generator_falesnych_dat.address())
+
+class Balik:
+  def get_info(self):
+    print(f"Příjemce balíku: {self.name}")
+    print(f"Balík doručte na adresu: {self.address}")
+
+  def __init__(self, name, address):
+    self.name = name
+    self.address = address
+
+balik1 = Balik(generator_falesnych_dat.name(), generator_falesnych_dat.address())
+
+print(balik1.get_info())
+
+# Proč mi to vypisuje None na poslední řádce?
