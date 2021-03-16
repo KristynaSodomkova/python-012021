55bd8ebbafd182221a76aaa6b6d7eb9433b19505
KristynaSodomkova <56554752+KristynaSodomkova@users.noreply.github.com>
2021-03-16 16:53:48 +0100

    opraveny ukol 17

--- 4/ukoly4/ukol17.py
+++ 4/ukoly4/ukol17.py
@@ -22,7 +22,8 @@ class Serial(Polozka):
     def get_info(self):
         return f"{self.name} se řadí k žánru {self.genre}. Série obsahuje {self.number_of_episodes} dílů. Jedna epizoda trvá {self.episode_length} min."
     def get_celkova_delka(self):
-        return self.episode_length * self.number_of_episodes
+        self.length = self.episode_length * self.number_of_episodes
+        return self.length
 
 class Uzivatel:
     def __init__(self, uzivatelske_jmeno):
@@ -34,12 +35,12 @@ class Uzivatel:
         zhlednute_porady = f"Uživatel {self.uzivatelske_jmeno} zhlédl tyto pořady: "
         for item in self.zhlednute_porady:
             zhlednute_porady += item.name + ", "
-            return zhlednute_porady
+        return zhlednute_porady
     def delka_sledovani(self):
         celkova_delka_sledovani = 0
         for item in self.zhlednute_porady:
             celkova_delka_sledovani += item.length
-            return f"Uživatel již strávil sledováním pořadů {celkova_delka_sledovani} min."
+        return f"Uživatel již strávil sledováním pořadů {celkova_delka_sledovani} min."
 
     def get_info(self):
         return f"Uživatel {self.uzivatelske_jmeno}."
@@ -49,14 +50,15 @@ stranger_things = Serial("Stranger Things", "sci-fi", 25, 45)
 moonrise_kingdom = Film("Moonrise Kingdom", "romantika/komedie", 95)
 nina_svehlova = Uzivatel("Nina Švehlová")
 
-print(stranger_things.get_info())
-print(moonrise_kingdom.get_info())
-print(moonrise_kingdom.get_celkova_delka())
+# print(stranger_things.get_info())
+# print(moonrise_kingdom.get_info())
+# (moonrise_kingdom.get_celkova_delka())
 print(stranger_things.get_celkova_delka())
-# print(nina_svehlova.pripocti_zhlednuti(10))
 print(nina_svehlova.get_info())
 nina_svehlova.zhledni_polozku(moonrise_kingdom)
-# print(nina_svehlova.get_porady())
+print(nina_svehlova.get_porady())
 print(nina_svehlova.delka_sledovani())
+nina_svehlova.zhledni_polozku(stranger_things)
+print(nina_svehlova.delka_sledovani())
+print(nina_svehlova.get_porady())
 
-# nejde mi dát celkovou dobu sledování do funkce get_info u uživatele... Help me please
