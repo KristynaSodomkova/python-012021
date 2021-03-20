08baf1d843b83ef8a160ec5930818e07f8e67fde
KristynaSodomkova <56554752+KristynaSodomkova@users.noreply.github.com>
2021-03-20 12:27:18 +0100

    upraveny ukol 18

--- 4/ukoly4/ukol18.py
+++ 4/ukoly4/ukol18.py
@@ -8,18 +8,24 @@ class Uchazec(Kontakt):
     def __init__(self, jmeno, email, datum_pohovoru):
         super().__init__(jmeno, email)
         self.datum_pohovoru = datetime.strptime(datum_pohovoru, "%d. %m. %Y")
-        self.zapis_z_pohovoru = ""
+        # self.zapis_z_pohovoru = ""
+
     def uloz_zapis(self, zapis_z_pohovoru):
         if datetime.now() < self.datum_pohovoru:
             return f"Chybně zadaný datum pohovoru."
         else:
-            self.zapis_z_pohovoru = input("Zápis z pohovoru: ")
+            # self.zapis_z_pohovoru = input("Zápis z pohovoru: ")
             self.zapis_z_pohovoru = zapis_z_pohovoru
             return f"Zápis byl uložen"
+    def get_info(self):
+        return f"zapis je: {self.zapis_z_pohovoru}"
+
+
 
 pohovor1 = Uchazec("Cyril Pažout", "pazout@seznam.cz", "19. 5. 2021")
 pohovor2 = Uchazec("Josef Horáček", "horacek@seznam.cz", "12. 2. 2021")
 
 print(pohovor1.uloz_zapis("bylo to super"))
 print(pohovor2.uloz_zapis("bylo to strašný"))
+print(pohovor2.get_info())
 
