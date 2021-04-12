ed271278f67c20cfb22b820c45930d17a11d6dcd
KristynaSodomkova <56554752+KristynaSodomkova@users.noreply.github.com>
2021-04-12 19:36:31 +0200

    opraveny ukol 35

--- 7/ukoly7/ukol35.py
+++ 7/ukoly7/ukol35.py
@@ -7,10 +7,11 @@ import matplotlib.pyplot as plt
 teploty["AvgTemperatureC"] = pytemperature.f2c(teploty["AvgTemperature"])
 
 mihelto = teploty[(teploty["City"] == "Miami Beach") | (teploty["City"] == "Helsinki") | (teploty["City"] == "Tokyo")]
+# šlo použít i isin
+mihelto = mihelto[["City", "AvgTemperatureC"]]
 
-ax = mihelto.plot("City", "AvgTemperatureC", kind="box", whis=[0, 100])
+ax = mihelto.boxplot(by="City", whis=[0, 100])
 ax.set_ylabel("Teploty ve stupních Celsia")
 ax.set_xlabel("Město")
 ax.set_title("Porovnání teplot Miami - Helsinki - Tokyo")
 plt.show()
-# no a tady mi to udělá graf jen s jedním boxem a nevim proč - vždyť tam mám všechna tři města
