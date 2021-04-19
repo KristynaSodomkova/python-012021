86ef61098f5ea4484227142221949df3edc95e0d
KristynaSodomkova <56554752+KristynaSodomkova@users.noreply.github.com>
2021-04-19 15:55:11 +0200

    uprava ukolu 38

--- pujcovna/pujcovna/urls.py
+++ pujcovna/pujcovna/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('katalog/', include("katalog.urls")),
+    path('seznam/', include("seznam.urls"))
 ]
