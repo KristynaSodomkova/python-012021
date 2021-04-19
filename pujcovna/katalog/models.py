215ac23f09d76abc3cb7116b6d2fdc88d52e408f
KristynaSodomkova <56554752+KristynaSodomkova@users.noreply.github.com>
2021-04-19 17:38:10 +0200

    hotove ukoly z 9

--- pujcovna/katalog/models.py
+++ pujcovna/katalog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Auto(models.Model):
+    registracnizn = models.CharField(max_length=20)
+    znackatyp = models.CharField(max_length=100)
+    najetekm = models.IntegerField()
+    datkontroly = models.DateField()
+
+class Zakaznik(models.Model):
+    jmenoprijm = models.CharField(max_length=100)
+    ridicprukaz = models.CharField(max_length=50)
+    datumnaroz = models.DateField()
+
+class Vypujcka(models.Model):
+    datumvypujcky = models.DateField()
+    datumkoncevypujcky = models.DateField()
+    cenavypujcky = models.IntegerField()
