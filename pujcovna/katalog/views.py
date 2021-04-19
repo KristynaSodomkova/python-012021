bacc121095c6d52219cc381d4c8b5eb36053d712
KristynaSodomkova <56554752+KristynaSodomkova@users.noreply.github.com>
2021-04-19 16:41:50 +0200

    hotovy ukol 40

--- pujcovna/katalog/views.py
+++ pujcovna/katalog/views.py
@@ -5,8 +5,17 @@ from django.views import View
 # Create your views here.
 class IndexView(View):
     def get(self, request):
-        return HttpResponse("Zde bude titulní stránka. Náš katalog najdete zde: "
-                            "<a href='http://localhost:8000/katalog/seznam/'>Katalog</a>"
+        return HttpResponse("<h1>Vítejte v autopůjčovně U Starýho Bouráka!</h1>"
+                            "<p>Tady Vám řekneme, proč si půjčit auto zrovna u nás</p>"
+                            "<h2>Užitečné odkazy:</h2>"
+                            "<a href='http://localhost:8000/katalog/seznam/'>Náš katalog</a>"
+                            "<p>Těšíme se na Vaši návštěvu!</p>"
+                            "<p>Kontakty:</p>"
+                            "<ul>"
+                                "<li>E-mail: ustaryhobouraka@seznam.cz</li>"
+                                "<li>Tel: 000 000 000</li>"
+                                "<li>Adresa: Za zatáčkou 123, Nikdynestůj</li>"
+                            "</ul>"
                             )
 
 class SeznamView(View):
